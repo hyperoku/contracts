cf4ba26c77f1cdda71debc4726a9e012ed1ce5bb
Matteo Casonato <matteocasonato@protonmail.com>
2022-10-12 18:49:58 +0200

    Init SudokuGenerator + Update RoundsManager

--- src/RoundsManager.sol
+++ src/RoundsManager.sol
@@ -1,107 +1,127 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ISudokuGenerator.sol";
 
-contract RoundsManager {
+error InvalidDifficulty();
+error RoundEndsSoon();
+error AnotherRoundAlreadyActive();
+
+contract RoundsManager is Ownable {
+
+    ISudokuGenerator public constant sudokuGenerator = ISudokuGenerator(0x5FbDB2315678afecb367f032d93F642f64180aa3);
 
     struct Game {
         uint64 id;
         uint32 round_id;
         address player;
-        uint64 start_blockNumber;
-        uint64 end_blockNumber;
         string sudoku;
         bytes32 solution;
-    }
-    
-    struct Round {
-        uint32 id;
-        mapping (uint64 => Game) games;
-        string difficulty;
-        uint64 start_blockNumber;
-        uint64 end_blockNumber;
+        uint256 start_blockNumber;
+        uint256 end_blockNumber;
     }
 
-    mapping (uint32 => Round) public rounds;
-    mapping (uint64 => Game) public games;
-    mapping (string => uint8) difficulty_values;
-    mapping (string => uint32) public last_active_round_ids; // DIFFICULTY -> ROUND ID
+    struct Round {
+        uint32 id;
+        string difficulty;
+        uint64[] game_ids;
+        uint256 start_blockNumber;
+        uint256 end_blockNumber;
+    }
+
+    mapping(uint32 => Round) public rounds;
+    mapping(uint64 => Game) public games;
+    mapping(string => uint8) difficulty_values;
+    mapping(string => uint32) public last_active_round_ids; // DIFFICULTY -> ROUND ID
     string[] public difficulty_names = ["EASY", "MEDIUM", "HARD"];
     uint32 public total_rounds;
     uint64 public total_games;
     uint32 public round_duration_in_blocks = 4320; // in Mumbai testnet, 1 block ~= 5 seconds -> 4320*5=21600s=6h
     uint8 public min_game_duration_in_blocks = 12; // 12*5=60s=1min
 
-    modifier difficultyExists(string memory _difficulty) {
-        require(
-            difficulty_values[_difficulty] != 0,
-            "Invalid difficulty."
-        ),
+    uint8 public constant MIN_DIFFICULTY_VALUE = 16;
+    uint8 public constant MAX_DIFFICULTY_VALUE = 64;
+
+    modifier difficultyExists(string calldata _difficulty) {
+        if (difficulty_values[_difficulty] == 0) 
+            revert InvalidDifficulty();
         _;
     }
 
-    constructor () public {
+    constructor() {
         difficulty_values["EASY"] = 37;
         difficulty_values["MEDIUM"] = 48;
         difficulty_values["HARD"] = 53;
     }
 
-    function createRound(string memory _difficulty) 
-        internal 
-        difficultyExists(_difficulty) 
-        returns (uint32 round_id) 
+    function createRound(string calldata _difficulty)
+        internal
+        difficultyExists(_difficulty)
+        returns (uint32 round_id)
     {
-        require(
-            rounds[last_active_rounds_ids[_difficulty]].end_blockNumber < block.number,
-            "There is already an active round for this difficulty."
-        );
+        if (rounds[last_active_round_ids[_difficulty]].end_blockNumber >= block.number)
+            revert AnotherRoundAlreadyActive();
+        uint64[] memory round_games;
         Round memory round = Round(
-            total_rounds, 
-            _difficulty, 
-            uint64(block.number), 
-            uint64(block.number + round_duration_in_blocks)
+            total_rounds,
+            _difficulty,
+            round_games,
+            block.number,
+            block.number + round_duration_in_blocks
         );
         total_rounds++;
         rounds[round.id] = round;
-        last_active_rounds_ids[_difficulty] = total_rounds;
+        last_active_round_ids[_difficulty] = total_rounds;
         return round.id;
     }
 
-    function createGame(string memory _difficulty) 
-        public 
-        difficultyExists(_difficulty) 
-        returns (string memory sudoku) 
+    function createGame(string calldata _difficulty)
+        public
+        difficultyExists(_difficulty)
+        returns (string memory sudoku)
     {
-        uint32 round_id = last_active_rounds_ids[_difficulty];
+        uint32 round_id = last_active_round_ids[_difficulty];
         if (rounds[round_id].end_blockNumber < block.number || round_id == 0) {
-            round_id = create_round(_difficulty);
-        } else {
-            require(
-                rounds[last_active_rounds_ids[_difficulty]].end_blockNumber > block.number + min_game_duration_in_blocks, 
-                "The round is ending soon."
-            );
+            round_id = createRound(_difficulty);
+        } 
+        else {
+            if (
+                rounds[last_active_round_ids[_difficulty]].end_blockNumber 
+                    <=
+                block.number + min_game_duration_in_blocks
+            )   
+                revert RoundEndsSoon();
         }
         uint64 game_id = total_games;
-        string memory sudoku;
         bytes32 solution;
-        (sudoku, solution) = generate_sudoku(_difficulty);
-        Game memory game = Game(game_id, round_id, msg.sender, block.number, 0, sudoku, solution);
+        (sudoku, solution) = sudokuGenerator.generateSudoku(
+            difficulty_values[_difficulty]
+        );
+        Game memory game = Game(
+            game_id,
+            round_id,
+            msg.sender,
+            sudoku,
+            solution,
+            block.number,
+            0
+        );
         games[game_id] = game;
-        rounds[round_id].games[game_id] = game;
+        rounds[round_id].game_ids.push(game_id);
         total_games++;
         return sudoku;
     }
 
-    function addNewDifficulty(string name, uint8 value) public onlyOwner {
-        require(
-            difficulty_values[name] == 0, 
-            "Difficulty already exists."
-        );
-        require(
-            value > 16 && value < 64, 
-            "Difficulty value must be between 16 and 64."
-        );
-        difficulty.push(name);
+    function addNewDifficulty(string calldata name, uint8 value)
+        public
+        onlyOwner
+    {
+        if (difficulty_values[name] != 0)
+            revert InvalidDifficulty();
+        if (value < MIN_DIFFICULTY_VALUE || value > MAX_DIFFICULTY_VALUE)
+            revert InvalidDifficulty();
+        difficulty_names.push(name);
         difficulty_values[name] = value;
     }
 
--- src/SudokuGenerator.sol
+++ src/SudokuGenerator.sol
@@ -1,5 +1,103 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract SudokuGenerator {
-    
+// import ISudokuGenerator
+import "./ISudokuGenerator.sol";
+
+contract SudokuGenerator is ISudokuGenerator {
+
+    // random0 function values: https://en.wikipedia.org/wiki/Linear_congruential_generator
+    uint16 constant a = 8121;
+    uint16 constant c = 28411;
+    uint32 constant m = 134456;
+
+    function generateRandom(uint64 _current_random_number)
+        internal
+        pure
+        returns (uint64 new_random_number, uint8 value) 
+    {
+        new_random_number = uint64(a * _current_random_number + c) % m;
+        value = uint8(uint256(keccak256(abi.encodePacked(new_random_number))) % 9);
+    }
+
+    function isValid(
+        uint8[9][9] memory _grid,
+        uint8 _row,
+        uint8 _col,
+        uint8 _num
+    ) internal pure returns (bool) {
+        for (uint8 x = 0; x < 9; x++) {
+            if (_grid[_row][x] == _num) {
+                return false;
+            }
+        }
+        for (uint8 x = 0; x < 9; x++) {
+            if (_grid[x][_col] == _num) {
+                return false;
+            }
+        }
+        uint8 _rowStart = _row - (_row % 3);
+        uint8 _colStart = _col - (_col % 3);
+        for (uint8 x = _rowStart; x < _rowStart + 3; x++) {
+            for (uint8 y = _colStart; y < _colStart + 3; y++) {
+                if (_grid[x][y] == _num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    function generateSudoku(uint8 difficulty)
+        external
+        pure
+        override
+        returns (string memory sudoku, bytes32 solution)
+    {
+        uint8[9][9] memory grid;
+        uint64 current_random_number = 123181231237;
+
+        // reset grid
+        for (uint8 i = 0; i < 9; i++) {
+            for (uint8 j = 0; j < 9; j++) {
+                grid[i][j] = 0;
+            }
+        }
+
+        // generate full sudoku grid
+        uint8 cell_value;
+        for (uint8 i = 0; i < 9; i++) {
+            for (uint8 j = 0; j < 9; j++) {
+                (current_random_number, cell_value) = generateRandom(current_random_number);
+                while (!isValid(grid, i, j, cell_value+1)) {
+                    (current_random_number, cell_value) = generateRandom(current_random_number);
+                }
+                grid[i][j] = cell_value;
+            }
+        }
+        delete cell_value;
+        solution = keccak256(abi.encodePacked(grid));
+
+        // remove random cells
+        uint8 row;
+        uint8 col;
+        for (uint8 i = 0; i < difficulty; i++) {
+            (current_random_number, row) = generateRandom(current_random_number);
+            (current_random_number, col) = generateRandom(current_random_number);
+            while (grid[row][col] == 0) {
+                (current_random_number, row) = generateRandom(current_random_number);
+                (current_random_number, col) = generateRandom(current_random_number);
+            }
+            grid[row][col] = 0;
+        }
+        delete row;
+        delete col;
+
+        // convert grid to string
+        for (uint8 i = 0; i < 9; i++) {
+            for (uint8 j = 0; j < 9; j++) {
+                sudoku = string(abi.encodePacked(sudoku, grid[i][j]));
+            }
+        }
+    }
 }
